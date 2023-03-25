d1cd5be47a5cabac4f0062fb9776c573b1c16446
Shinaya123 <83915008+Shinaya123@users.noreply.github.com>
2023-03-26 01:15:23 +0530

    Add files via upload

---
+++ read_video.py
@@ -0,0 +1,31 @@
+import cv2
+
+vid= cv2.VideoCapture(0)
+
+if(vid.isOpened()==False):
+    print('Unable to Read the Feed')
+
+height=int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(height)
+
+width=int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(width)
+
+fps=int(vid.get(cv2.CAP_PROP_FPS))
+print(fps)
+
+out=cv2.VideoWriter('image.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, (width,height))
+
+
+while(True):
+    ret,frame= vid.read()
+    cv2.imshow('webcam', frame)
+    out.write(frame)
+    if cv2.waitKey(1)==32:
+        break
+    
+vid.release()
+out.release()
+cv2.destroyAllWindows()
+
+        
